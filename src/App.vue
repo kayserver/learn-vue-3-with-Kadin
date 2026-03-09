a115bc7140c8b07c2fc35a477193b6b133ddba32
Kadin Lee-Smith <kadinlee-smith@Kadins-MacBook-Air.local>
2026-03-09 18:59:37 -0500

    Pass data to descendant components with provide and inject.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>Hello, Vue!</div>
+  <h1>Message: {{ message }}</h1>
+  <div style="background-color: orange; padding-left: 10px">
+    <ComponentA></ComponentA>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, provide, readonly } from 'vue'
+import ComponentA from './ComponentA.vue'
+//import component A
+let message = ref('Hello, Provide/inject!')
 
-<style lang="scss" scoped></style>
+function updateMessage() {
+  message.value = 'Hello, Provide/inject! Updated.'
+}
+// first arg is the injection key
+provide('msg', { message, updateMessage })
+// components cant mutate the counts
+let count = ref(0)
+provide('read-only-count', readonly(count))
+</script>
+
+<style scoped></style>
---
+++ src/ComponentB.vue
@@ -0,0 +1,19 @@
+<template>
+  <div style="background-color: lightblue">
+    <h2>This is Component B.</h2>
+    {{ messageFromApp }}
+    <button @click="updateMessage">Update message</button>
+  </div>
+</template>
+
+<script setup>
+import { inject } from 'vue'
+//define a varible 
+// default message is not required but helps 
+let { message: messageFromApp, updateMessage } = inject(
+  'msg',
+  'default message'
+)
+</script>
+
+<style scoped></style>
