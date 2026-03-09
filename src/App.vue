6a8ee104c641d2bd2c78057c9c7bec03b5bb9c04
Kadin Lee-Smith <kadinlee-smith@Kadins-MacBook-Air.local>
2026-03-09 16:47:55 -0500

    Introuduction to Vue Components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
-  <div>Hello, Vue!</div>
+  <ButtonCounter></ButtonCounter>
+  <ButtonCounter></ButtonCounter>
+
+  <button-counter></button-counter>
+
+  <hr />
+
+  <div style="background-color: orange; padding: 5px">
+    This is the root component.
+    <ComponentA></ComponentA>
+    <ComponentA></ComponentA>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import ButtonCounter from './ButtonCounter.vue'
+import ComponentA from '@/ComponentA.vue'
+//this component is used as a custom element
+// each time you use a button counter, then a new button instance is cerated
+// pascal case differenciates components defined by the devloper
 
-<style lang="scss" scoped></style>
+</script>
+
+<style scoped></style>
